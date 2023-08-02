04ac980eab98932e7311b2b0b9bf37d998338b57
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-03 01:02:31 +0900

    add: up, error

--- GunPey.py
+++ GunPey.py
@@ -1,19 +1,9 @@
 import tkinter
+import random
+import time
 
-cursor_x = 0
-cursor_y = 0
-mouse_x = 0
-mouse_y = 0
-mouse_c = 0
-
-def mouse_move(e):
-    global mouse_x, mouse_y
-    mouse_x = e.x
-    mouse_y = e.y
-
-def mouse_press(e):
-    global mouse_c
-    mouse_c = 1
+upTimer = 0
+insertnow = 0
 
 gunpey = [
     [0, 0, 0, 0, 0,],
@@ -34,33 +24,64 @@ def draw_panel():
             if gunpey[y][x] > 0:
                 cvs.create_image(x*24+21, y*16+57, image=img_panel[gunpey[y][x]], tag="PANEL")
 
+def in_panel():
+    global slot,insertnow
+    insertnow = 1
+    cvs.create_image(69, 201, image=slot, tag="SLOT")
+    insert = [0, 0, 0, 0, 0]
+    for x in range(5):
+        insert[x] = random.randint(0, 4)
+    for x_point in range(60, 0, -1):
+        cvs.delete("INSERT")
+        for x in range(5):
+            cvs.create_image(x*24+21, 201, image=img_panel[insert[x]], tag="INSERT")
+            print("hey"+str(x*24+21+x_point) + ":" + str(insertnow) + ":" + str(insert[x]))
+        time.sleep(0.1)
+        print(x_point)    
+    cvs.delete("INSERT")
+    for x in range(5):
+        gunpey[9][x] = insert[x] # Insert the new panel 9,193
+    cvs.delete("SLOT")
+    insertnow = 0
+
+def up_panel():
+    for y in range(1, 10):
+        for x in range(5):
+            gunpey[y-1][x] = gunpey[y][x]
+            gunpey[y][x] = 0
+        
+def game_over():
+    print("GameOver")
+
 def game_main():
-    global cursor_x, cursor_y, mouse_c
-    if 9 <= mouse_x and mouse_x < 9+24*5 and 57 <= mouse_y and mouse_y < 49+16*9:
-        cursor_x = int((mouse_x-9)/24)
-        cursor_y = int((mouse_y-57)/16)
-        if mouse_c == 1:
-            mouse_c = 0
-            tmp = 0
-            tmp = gunpey[cursor_y][cursor_x]
-            gunpey[cursor_y][cursor_x] = gunpey[cursor_y+1][cursor_x]
-            gunpey[cursor_y+1][cursor_x] = tmp
+    global upTimer, insertnow
     cvs.delete("CURSOR")
-    cvs.create_image(cursor_x*24+21, cursor_y*16+65, image=cursor, tag="CURSOR")
     cvs.delete("PANEL")
-    draw_panel()
+    if insertnow == 0:
+        draw_panel()
+    upTimer += 1
+    print(upTimer)
+    if upTimer >= 33:
+        sum_panel = 0
+        for x in range(5):
+            sum_panel += gunpey[0][x]
+        if sum_panel == 0:
+            up_panel()
+            in_panel()
+        else:
+            game_over()
+        upTimer = 0
     root.after(100, game_main)
 
 root = tkinter.Tk()
 root.title("GUNPEY")
 root.resizable(False, False)
-root.bind("<Motion>", mouse_move)
-root.bind("<ButtonPress>", mouse_press)
 cvs = tkinter.Canvas(root, width=146, height=226)
 cvs.pack()
 
 bg = tkinter.PhotoImage(file="./image/GupPey_GameBoard_Stage.png")
 cursor = tkinter.PhotoImage(file="./image/GupPey_GameBoard_Cursor.png")
+slot = tkinter.PhotoImage(file="./image/GupPey_GameBoard_Slot.png")
 img_panel = [
     None,
     tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_Z.png"),
