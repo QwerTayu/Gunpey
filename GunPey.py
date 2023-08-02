fbc6f34d019b4533d325b74aea1bd5f5f334b69c
QwerTayu <114202555+QwerTayu@users.noreply.github.com>
2023-08-03 00:55:47 +0900

    add cursor (#3)

--- GunPey.py
+++ GunPey.py
@@ -1,40 +1,33 @@
 import tkinter
 
+cursor_x = 0
+cursor_y = 0
+mouse_x = 0
+mouse_y = 0
 
-gunpey = [
-    [0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0,],
-    [0, 0, 0, 4, 0,],
-    [0, 0, 0, 0, 0,],
-    [1, 2, 3, 4, 0,],
-    [0, 0, 0, 0, 0,]
-]
+def mouse_move(e):
+    global mouse_x, mouse_y
+    mouse_x = e.x
+    mouse_y = e.y
 
-def draw_panel():
-    for y in range(10):
-        for x in range(5):
-            if gunpey[y][x] > 0:
-                cvs.create_image(x*24+21, y*16+57, image=img_panel[gunpey[y][x]])
+def game_main():
+    global cursor_x, cursor_y
+    if 9 <= mouse_x and mouse_x < 9+24*5 and 57 <= mouse_y and mouse_y < 49+16*9:
+        cursor_x = int((mouse_x-9)/24)
+        cursor_y = int((mouse_y-57)/16)
+    cvs.delete("CURSOR")
+    cvs.create_image(cursor_x*24+21, cursor_y*16+65, image=cursor, tag="CURSOR")
+    root.after(10, game_main)
 
 root = tkinter.Tk()
 root.title("GUNPEY")
 root.resizable(False, False)
+root.bind("<Motion>", mouse_move)
 cvs = tkinter.Canvas(root, width=146, height=226)
 cvs.pack()
 
 bg = tkinter.PhotoImage(file="./image/GupPey_GameBoard_Stage.png")
-img_panel = [
-    None,
-    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_Z.png"),
-    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_N.png"),
-    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_A.png"),
-    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_V.png"),
-]
-
+cursor = tkinter.PhotoImage(file="./image/GupPey_GameBoard_Cursor.png")
 cvs.create_image(73, 113, image=bg)
-draw_panel()
+game_main()
 root.mainloop()
