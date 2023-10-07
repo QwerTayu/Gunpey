8d82da2e1f6de14f269b6c561166707c9f5ed737
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-10-07 11:37:56 +0900

    ✨ パネルを挿入するときにスロットを作成

--- GunPey.py
+++ GunPey.py
@@ -1,10 +1,13 @@
 import tkinter
+import random
+import time
 
 cursor_x = 0
 cursor_y = 0
 mouse_x = 0
 mouse_y = 0
 mouse_c = 0
+gameTime = 0
 
 def mouse_move(e):
     global mouse_x, mouse_y
@@ -22,11 +25,30 @@ normalPanel = [
     [0, 0, 0, 0, 0,],
     [0, 0, 0, 0, 0,],
     [0, 0, 0, 0, 0,],
-    [0, 0, 0, 7, 0,],
     [0, 0, 0, 0, 0,],
-    [1, 3, 5, 7, 0,],
+    [0, 0, 0, 0, 0,],
+    [0, 0, 0, 0, 0,],
     [0, 0, 0, 0, 0,]
 ]
+
+def new_panel():
+
+    weights = [8, 1, 1, 1, 1]
+
+    isPanelExist = False
+    
+    while isPanelExist == False:
+        newPanel = random.choices(range(5), weights=weights, k=5)
+        for x in range(5):
+            if newPanel[x] != 0:
+                isPanelExist = True
+                break
+
+    for y in range(1, 10):
+        for x in range(5):
+            normalPanel[y-1][x] = normalPanel[y][x]
+
+    normalPanel[9] = newPanel
 
 def draw_panel(panel):
     for y in range(10):
@@ -34,7 +56,7 @@ def draw_panel(panel):
             if panel[y][x] > 0:
                 cvs.create_image(x*24+21, y*16+57, image=img_panel[panel[y][x]], tag="PANEL")
 
-def game_main():
+def handleClickChange(normalPanel):
     global cursor_x, cursor_y, mouse_c
     if 9 <= mouse_x and mouse_x < 9+24*5 and 57 <= mouse_y and mouse_y < 49+16*9:
         cursor_x = int((mouse_x-9)/24)
@@ -45,11 +67,22 @@ def game_main():
             tmp = normalPanel[cursor_y][cursor_x]
             normalPanel[cursor_y][cursor_x] = normalPanel[cursor_y+1][cursor_x]
             normalPanel[cursor_y+1][cursor_x] = tmp
+
+def game_main():
+    global cursor_x, cursor_y, mouse_c, gameTime
+    handleClickChange(normalPanel)
     cvs.delete("CURSOR")
     cvs.create_image(cursor_x*24+21, cursor_y*16+65, image=cursor, tag="CURSOR")
     cvs.delete("PANEL")
     draw_panel(normalPanel)
+    cvs.tag_raise("SLOT")
     root.after(100, game_main)
+    gameTime += 1
+    if gameTime % (10 * 10) == 0: # 10秒ごとにパネルを生成
+        new_panel()
+        cvs.create_image(2*24+21, 8.5*16+65, image=slot, tag="SLOT")
+    if gameTime % (10 * 10) == 3:
+        cvs.delete("SLOT") 
 
 root = tkinter.Tk()
 root.title("GUNPEY")
@@ -64,14 +97,15 @@ cursor = tkinter.PhotoImage(file="./image/GupPey_GameBoard_Cursor.png")
 img_panel = [
     None,
     tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_Z_0.png"),
-    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_Z_1.png"),
     tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_N_0.png"),
-    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_N_1.png"),
     tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_A_0.png"),
-    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_A_1.png"),
     tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_V_0.png"),
+    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_Z_1.png"),
+    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_N_1.png"),
+    tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_A_1.png"),
     tkinter.PhotoImage(file="./image/Panels/GupPey_GameBoard_Panels_V_1.png"),
 ]
+slot = tkinter.PhotoImage(file="./image/GupPey_GameBoard_Slot.png")
 
 cvs.create_image(73, 113, image=bg)
 game_main()
